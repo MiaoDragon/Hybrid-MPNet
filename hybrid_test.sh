26ccf0318f755dced2ab2f8bb5c752d0cd021da7
MiaoDragon <innocentmyl@gmail.com>
2019-02-21 21:05:41 -0800

    naming issue

--- hybrid_test.sh
+++ hybrid_test.sh
@@ -1,4 +1,4 @@
-python3 cont_test.py --model_path ../hybrid_res/global/s2d/no_include/ \
+python3 hybrid_test.py --model_path ../hybrid_res/global/s2d/no_include/ \
 --no_env 100 --no_motion_paths 4000 --grad_step 1 --learning_rate 0.01 \
 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 0 --data_path ../data/simple/ \
