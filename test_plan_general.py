42c0c7a4706634bd26cf5ff4d0890c9a5adb65e5
MiaoDragon <innocentmyl@gmail.com>
2019-02-22 11:33:24 -0800

    change path length from 80 to 50. When SteerTo, if the two vertices are closer than threshold, then assume they are fine

--- test_plan_general.py
+++ test_plan_general.py
@@ -1,4 +1,6 @@
 from plan_general import *
+from Model.GEM_end2end_model import End2EndMPNet
+from utility import *
 def IsInCollision(x,obc):
     size = 5.0
     s=np.zeros(2,dtype=np.float32)
@@ -17,7 +19,8 @@ def IsInCollision(x,obc):
 print('steerTo test...')
 start = torch.tensor([0.,-5.])
 end = torch.tensor([4.99,0.])
-obc = np.array([0.,0.])
+obc = np.array([0.,0.])  # define a box at origin of size 5
+size = 5.
 print(steerTo(start, end, obc, IsInCollision, step_sz=0.01))
 # test feasibility check
 print('feasibility test...')
@@ -28,5 +31,36 @@ print('lvc...')
 path = [torch.tensor([0.,6.]), torch.tensor([0.,5.]),torch.tensor([0.,4.]),torch.tensor([0.,3.]),torch.tensor([-3.,3.]),torch.tensor([-3.,-3.]),
         torch.tensor([0.,-6.])]
 print(lvc(path, obc, IsInCollision, step_sz=0.01))
+pc_low = [[obc[0]-size/2,obc[1]-size/2] for i in range(1400)]
+pc_low = np.array(pc_low)
 
+pc_high = [[obc[0]+size/2,obc[1]+size/2] for i in range(1400)]
+pc_high = np.array(pc_high)
+obs = np.random.uniform(low=pc_low, high=pc_high)
+obs = torch.from_numpy(obs).float().flatten()
+print(obs)
+world_size=20
 # test neural replan
+mpnet = End2EndMPNet(mlp_input_size=28+4, output_size=2, AEtype='deep', \
+             n_tasks=1, n_memories=10, memory_strength=0.5, grad_step=1)
+path = [torch.tensor([0.,6.]).float(), torch.tensor([0.,-6.]).float()]
+print(path)
+DEFAULT_STEP = 0.05
+step_sz = DEFAULT_STEP
+for t in range(11):
+    if (t == 2):
+        step_sz = 0.04
+    elif (t == 3):
+        step_sz = 0.03
+    elif (t > 3):
+        step_sz = 0.02
+    print('%d-th planning...' % (t))
+    unnormalize_func = lambda x: unnormalize(x, world_size)
+    path = neural_replan(mpnet, path, obc, obs, IsInCollision, unnormalize_func, step_sz=DEFAULT_STEP)
+    path = lvc(path, obc, IsInCollision, step_sz=step_sz)
+    print(path)
+    if feasibility_check(path,obc, IsInCollision, step_sz=0.01):
+        fp = True
+        print('feasible, ok!')
+        break
+print(path)
