174ea202c29cce75078d445b9930883c2d05ac78
MiaoDragon <innocentmyl@gmail.com>
2019-02-22 14:17:27 -0800

    dumb bug

--- hybrid_test.sh
+++ hybrid_test.sh
@@ -10,6 +10,6 @@ python3 hybrid_test.py --model_path ../hybrid_res/global/s2d/no_include/ \
 --start_epoch 0 --memory_type res --env_type s2d --world_size 20 \
 --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 \
 --seen_N 10 --seen_NP 200 --seen_s 0 --seen_sp 4000 \
---unseen_N 10 --seen_NP 200 --seen_s 100 --seen_sp 0
+--unseen_N 10 --unseen_NP 200 --unseen_s 100 --unseen_sp 0
 # seen: 100, 200, 0, 4000
 # unseen: 10, 2000, 100, 0
