872b74f1d1280c85698fbaf2d647fb6e6090d2fa
MiaoDragon <innocentmyl@gmail.com>
2019-02-24 23:51:38 -0800

    output size should be 3 for r3d

--- hybrid_train_global_demo_c2d.sh
+++ hybrid_train_global_demo_c2d.sh
@@ -4,4 +4,4 @@ python3 hybrid_train_global_demo.py --model_path ../hybrid_res/global/c2d/no_inc
 --n_tasks 1 --device 1 --freq_rehersal 100 --batch_rehersal 100 \
 --start_epoch 0 --MAX_NEURAL_REPLAN 11 --pretrain_path 200 --data_path /media/arclabdl1/HD1/Ahmed/r-2d/ \
 --include_suc_path 0 --world_size 20 --env_type c2d \
---total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32
+--total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2
