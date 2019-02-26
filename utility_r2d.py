9ede7465474b8e0557542c2f2a5471bebdb8bde5
MiaoDragon <innocentmyl@gmail.com>
2019-02-26 15:19:34 -0800

    debug utility_r2d

--- utility_r2d.py
+++ utility_r2d.py
@@ -1,8 +1,9 @@
 import torch
 from torch.autograd import Variable
 import copy
+import numpy as np
 def normalize(x, bound):
     # normalize to -1 ~ 1  (bound can be a tensor)
-    return x / bound
+    return x / torch.tensor(bound)
 def unnormalize(x, bound):
-    return x * bound
+    return x * torch.tensor(bound)
