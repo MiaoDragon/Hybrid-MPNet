a3130eba6978387ec411a639e49f0827ed874dc9
MiaoDragon <innocentmyl@gmail.com>
2019-02-27 10:21:16 -0800

    change c2d hybrid to 1000 pretrain, to see if it is fast

--- hybrid_train_global_demo_c2d.sh
+++ hybrid_train_global_demo_c2d.sh
@@ -2,6 +2,6 @@ python3 hybrid_train_global_demo.py --model_path ../hybrid_res/global/c2d/no_inc
 --no_env 100 --no_motion_paths 4000 --grad_step 1 \
 --num_epochs 1 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 1 --freq_rehersal 100 --batch_rehersal 100 \
---start_epoch 0 --MAX_NEURAL_REPLAN 11 --pretrain_path 200 --data_path /media/arclabdl1/HD1/Ahmed/r-2d/ \
+--start_epoch 0 --MAX_NEURAL_REPLAN 11 --pretrain_path 1000 --data_path /media/arclabdl1/HD1/Ahmed/r-2d/ \
 --include_suc_path 0 --world_size 20 --env_type c2d \
 --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32 --output_size 2
