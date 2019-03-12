fb3f556409b912459cb14bfe71ced1070610b267
MiaoDragon <innocentmyl@gmail.com>
2019-03-11 23:08:25 -0700

    don't normalize obstacles, but normalize state

--- utility_r2d.py
+++ utility_r2d.py
@@ -7,7 +7,8 @@ def normalize(x, bound):
     bound = torch.tensor(bound)
     if len(x[0]) != len(bound):
         # then the proceding is obstacle
-        x[:,:-2*len(bound)] = x[:,:-2*len(bound)] / bound[0]
+        # don't normalize obstacles
+        #x[:,:-2*len(bound)] = x[:,:-2*len(bound)] / bound[0]
         x[:,-2*len(bound):-len(bound)] = x[:,-2*len(bound):-len(bound)] / bound
         x[:,-len(bound):] = x[:,-len(bound):] / bound
     else:
@@ -20,7 +21,8 @@ def unnormalize(x, bound):
     bound = torch.tensor(bound)
     if len(x) != len(bound):
         # then the proceding is obstacle
-        x[:-2*len(bound)] = x[:-2*len(bound)] * bound[0]
+        # don't normalize obstacles
+        #x[:-2*len(bound)] = x[:-2*len(bound)] * bound[0]
         x[-2*len(bound):-len(bound)] = x[-2*len(bound):-len(bound)] * bound
         x[-len(bound):] = x[-len(bound):] * bound
     else:
