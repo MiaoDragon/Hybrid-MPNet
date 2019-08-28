c3547a921e1595588c92c74c37f160052fcd281b
MiaoDragon <innocentmyl@gmail.com>
2019-08-28 13:36:03 -0700

    added collision check for testing

--- plan_s2d.py
+++ plan_s2d.py
@@ -5,10 +5,11 @@ def IsInCollision(x,obc):
     s=np.zeros(2,dtype=np.float32)
     s[0]=x[0]
     s[1]=x[1]
+
     for i in range(0,7):
         cf=True
         for j in range(0,2):
-            if abs(obc[i][j] - s[j]) > size/2.0:
+            if abs(obc[i][j] - s[j]) > size/2.0 and s[j] < 20.0 and s[j] > -20.0:
                 cf=False
                 break
         if cf==True:
