3809875c7f824010c99bae15913db3eee90b8e4e
MiaoDragon <innocentmyl@gmail.com>
2019-09-23 11:36:29 -0700

    use small dataset first to see if it is working

--- exp/hybrid_train_global_demo_s2d.sh
+++ exp/hybrid_train_global_demo_s2d.sh
@@ -1,9 +1,10 @@
 cd ..
-python3 hybrid_train_global_demo.py --model_path ../hybrid_res/global/s2d/no_include/ --AEtype_deep 1 \
---no_env 100 --no_motion_paths 4000 --grad_step 1 \
+python3 hybrid_train_global_demo.py --model_path /media/arclabdl1/HD1/YLmiao/results/hybrid_res/global/s2d/ --AEtype_deep 1 \
+--no_env 10 --no_motion_paths 40 --grad_step 1 \
 --num_epochs 1 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 0 --freq_rehersal 100 --batch_rehersal 100 \
 --start_epoch 0 --MAX_NEURAL_REPLAN 11 --pretrain_path 200 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ \
 --include_suc_path 0 --world_size 20 --env_type s2d \
 --total_input_size 2804 --AE_input_size 2800 --mlp_input_size 32
 cd exp
+#100 x 4000
